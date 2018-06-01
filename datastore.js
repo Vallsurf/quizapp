d78da890e05089d0a86262b2cf77156b70cc50fa
valls <vallsurf@gmail.com>
2018-06-01 11:01:31 -0700

    inital commit

---
+++ datastore.js
@@ -0,0 +1,36 @@
+
+
+const STORE = [
+  {id: 1, 
+  name: "On July 4th, 2016, which player decided to take the \"Hardest Road\" and sign with the winningest team in regular season NBA history? ", 
+  answers: ["James Harden", "Michael Jordan", "Kevin Durant",  "Kobe Bryant"],   
+  correct: "Kevin Durant" 
+  
+  },
+
+  {id: 2, 
+  name: "When this NBA player wins, he \"can win with anyone.\" But when he loses, he \"doesn\'t have the supporting cast he needs.\" ", 
+  answers: ["Lebron James", "Malik Monk", "Meek Mill", "Kevin Durant"],
+  correct: "Kevin Durant"
+
+  },
+
+  {id: 3,
+  name: "Which player has earned the respectable and honorable symbol of a cupcake?",  
+  answers: ["Charles Barkley", "Glen Davis", "Ray Felton", "Kevin Durant"], 
+  correct: "Kevin Durant"
+  
+  },
+
+  {id: 4,name: "Which player was up 3-1 in the Western Conference Finals and lost, and ended up joining the exact team he lost to 3 months later?",  
+  answers: ["Larry Bird", "Steph Curry", "Kevin Durant",  "Chris Paul"], 
+  correct: "Kevin Durant"
+  
+  },
+
+  {id: 5,
+  name: "On May 15th, 2018, the Wall Street Journal proclaimed that this player RUINED the NBA?",  
+  answers: ["James Harden", "Steph Curry",  "Kevin Durant", "LeBron James"], 
+  correct: "Kevin Durant"
+  },
+];
---
+++ index.js
@@ -0,0 +1,141 @@
+
+
+let questionCount = 0;
+let questionTodisplay = 0;
+let score = 0;  
+
+//when intro button is clicked, hides welcome title 
+function hideStart () {
+$('.startButton').on('click', function(event) {
+$(this).closest('.quizStart').remove();
+$("body").css({"background-image":"none"}); 
+questionTodisplay ++;
+renderQuestionnumber (questionTodisplay) ; 
+renderQuizform(STORE, questionCount);  
+  });
+} 
+
+//displays Question Number 
+function renderQuestionnumber (questionTodisplay) {
+$('.questionNumber').html(`${questionTodisplay}`);
+} 
+
+//displays Score
+function renderScore (score) {
+$('.score').html(`${score}`);
+} 
+
+//Displays question and choices
+function renderQuizform (STORE, numberofquestion) {
+  let questionnumber = STORE[numberofquestion]; 
+  let questionarray = questionnumber.answers; 
+$('.questionAnswerForm').removeAttr('hidden');
+$('.questionAnswerForm').html(`<h1 class="question">${questionnumber.name} </h1>
+ <form>
+    <fieldset>
+    <legend>Answer Choices</legend> 
+    <label class="answerOption">
+    <input type="radio" value="${questionarray[0]}" name="answer" required>
+    <span>${questionarray[0]}</span>
+    </label> 
+      <label class="answerOption">
+    <input type="radio" value="${questionarray[1]}" name="answer" required>
+    <span>${questionarray[1]}</span>
+    </label> 
+      <label class="answerOption">
+    <input type="radio" value="${questionarray[2]}" name="answer" required>
+    <span>${questionarray[2]}</span>
+    </label> 
+      <label class="answerOption">
+    <input type="radio" value="${questionarray[3]}" name="answer" required>
+    <span>${questionarray[3]}</span>
+    </label> 
+    <button type="submit" class="submitButton">Submit</button>
+    </fieldset>
+</form>`)
+  
+}
+
+//function to determine if answer is correct or not when clicked
+function CheckAnswer () {
+$('.questionAnswerForm').on('submit', function(event) {
+  event.preventDefault();
+$('.AnswerResponse').removeAttr('hidden');
+let selected = $('input:checked');
+let answer = selected.val();
+//var choicenum = $(this).attr("id");
+console.log(`${answer}`);
+console.log (`correct is ${questionCount}`) 
+if (answer === STORE[questionCount].correct) 
+ {rightAnswer();}
+ else {wrongAnswer();}
+  });
+} 
+
+//Displays next question after clicking answer feedback view button
+function nextButtonclick () {
+$('.AnswerResponse').on('click', '.nextButton', function(event) {
+  var doneornot = questionCount ; 
+  $('.AnswerResponse').hide();
+  if (doneornot +1 < STORE.length) {
+questionTodisplay ++;
+questionCount ++;
+console.log(`question count is ${questionCount}`)
+renderQuestionnumber (questionTodisplay) ; 
+renderQuizform(STORE, questionCount);  
+  }
+  else {  
+   $('.AnswerResponse').attr("hidden", "true");
+  $('.QuizDone').show()
+   $('.QuizDone').html(`<h1> You Scored  ${score} points out of 5! </h1> <img src = "https://media.giphy.com/media/rrYfvW8z0v0qc/giphy.gif" /> 
+   <h1>God Bless Kevin Durant!</h1>
+   <button type="button" class="TryAgainbutton">Play Again!</button>`) }
+  });
+} 
+
+//Logic for when right answer is chosen
+function rightAnswer () {
+  score ++;
+  renderScore (score)
+$('.questionAnswerForm').attr("hidden","true")
+$('.AnswerResponse').show();
+$('.AnswerResponse').html(`<h1>You Got It Right! </h1> <img src="https://media.giphy.com/media/10ERZqYioLWJ6U/giphy.gif" />
+<button type="button" class="nextButton">I'm Heatin Up!</button>`)
+
+} 
+
+//Logic for when wrong answer is chosen 
+function wrongAnswer () {
+$('.questionAnswerForm').attr("hidden","true")  
+$('.AnswerResponse').show();
+$('.AnswerResponse').html(`<h1>You Got It Wrong! The correct answer was ${STORE[questionCount].correct}! </h1><img src="https://media.giphy.com/media/O5NyCibf93upy/giphy.gif" />
+<button type="button" class="nextButton">You Miss 100 Percent off the Shots You Don't Take, Rack em Up!</button>`)
+} 
+
+//Resets Quiz 
+function resetQuiz () {
+$('.QuizDone').on('click', '.TryAgainbutton', function(event) {
+$('.QuizDone').hide()
+questionCount = 0 
+score = 0 
+questionTodisplay = 1;
+renderQuestionnumber (questionTodisplay) ; 
+renderScore (score); 
+renderQuizform(STORE, questionCount);
+
+});
+}
+
+//initiates Quiz
+function handleQuiz() {
+  hideStart();
+  nextButtonclick (questionCount); 
+  CheckAnswer (); 
+  renderQuestionnumber (questionTodisplay);
+  renderScore (score);
+  resetQuiz () ; 
+}
+
+
+// when the page loads, call `handleQuiz`
+$(handleQuiz);
